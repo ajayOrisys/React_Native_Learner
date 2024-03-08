0fa2b6eaf569cfd371ea5608bbf328f84e6481ec
ajayOrisys <ajay.sajikumar@orisysacademy.com>
2024-03-08 15:16:39 +0530

    change made

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -23,7 +23,7 @@ const Welcome = () => {
 	return (
 		<View>
 			<View style={styles.container}>
-				<Text style={styles.userName}>Hello Adrian !</Text>
+				<Text style={styles.userName}>Hello Adrian</Text>
 				<Text style={styles.welcomeMessage}>Find your perfect job</Text>
 			</View>
 
